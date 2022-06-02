2380501cd3a27cdda826fd27bddcf0c222d6a7f7
FedoTT <urkytsk3@gmail.com>
2022-06-02 16:40:55 +0800

    added field validators

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,19 @@
                 {{ form.form.password }}
             </pre>
 
-            <div class="form-control">
+            <div class="form-control " :class="{invalid: !form.form.email.valid}">
                 <label for="email">Email</label>
                 <input type="email" id="email" v-model="form.form.email.value">
+                <small v-if="form.form.email.errors.required">Введите почту</small>
             </div>
 
-            <div class="form-control">
+            <div class="form-control" :class="{invalid: !form.form.password.valid}">
                 <label for="password">Password</label>
                 <input type="password" id="password" v-model="form.form.password.value">
+                <small v-if="form.form.password.errors.required">Введите пароль</small>
+                <small v-else-if="form.form.password.errors.minLength">
+                    Минимальное коллличество символов не может быть меньша 8 сейчас символов: {{form.form.password.value.length}}
+                </small>
             </div>
 
             <button class="btn primary" type="submit">Submit</button>
@@ -48,7 +53,7 @@
             function submit() {}
 
             return {
-                form, submit
+                form, submit,minLength
             }
         }
 
--- src/use/field.js
+++ src/use/field.js
@@ -1,11 +1,27 @@
-import {ref} from 'vue'
+import {ref, reactive, watch} from 'vue'
+
+const not = val => !val
 
 export function useField(field) {
     const valid = ref(true)
     const value = ref(field.value)
+    const errors = reactive({})
+
+    const reassign = val => {
+        valid.value = true
+
+        Object.keys(field.validators ?? {}).map(name => {
+            const isValid = field.validators[name](val)
+            errors[name] = not(isValid)
+            if (not(isValid)) {
+                valid.value = false
+            }
+        })
+    }
 
 
+    watch(value, reassign)
+    reassign(value)
 
-
-    return {valid, value}
+    return {valid, value, errors}
 }
